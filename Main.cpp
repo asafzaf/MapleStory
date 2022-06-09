cc1b979573901b248825a7e4f11e40b55e5c4895
asafzaf <104023823+asafzaf@users.noreply.github.com>
2022-06-09 22:07:46 +0300

    Add files via upload

--- Main.cpp
+++ Main.cpp
@@ -1,11 +1,14 @@
-
 #include "User.hpp"
 #include "Enemy.hpp"
 #include <iostream>
 
 using namespace std;
+
+// for option the there are characters //
 char C1[] = "Jinx"; char C2[] = "Elie"; char C3[] = "Joul"; char C4[] = "Ash"; char C5[] = "Jack";
 Character characters[5] = {Character(C1,Warrior), Character(C2,Magician), Character(C3,Rouge), Character(C4,Archer), Character(C5,Pirate) } ;
+
+// generating enemies //
 char E1_name[4] = "avi"; char E2_name[4] = "eli"; char E3_name[6] = "jakob"; char E4_name[6] = "david"; char E5_name[5] = "josh"; char E6_name[7] = "barbur"; char E7_name[7] = "sandal";
 Enemy enemies[7] = {Enemy(E1_name),
                     Enemy(E2_name, 15, 15, 15),
@@ -15,11 +18,18 @@ Enemy enemies[7] = {Enemy(E1_name),
                     Enemy(E6_name, 65, 30, 50),
                     Enemy(E7_name, 50, 35, 30)};
 int numberofenemies = 7;
+
+// login details //
 char main_user_name[] = "YuviVerse";
 char main_user_pass[] = "TwitchTTV";
+
+// options for starting :
+
 //User main_user(main_user_name, main_user_pass);
 //User main_user(main_user_name, main_user_pass,characters[0]);
 User main_user(main_user_name, main_user_pass,characters);
+
+
 int main()
 {
     Character new_character;
@@ -27,7 +37,7 @@ int main()
     cout << "            ````````" << endl;
 
    // main_user.login();
-    if (!main_user.characterCount()) // create new character
+    if (!main_user.characterCount()) // create new character if the user generated without one
     { 
 
         char *character_name = new char;
@@ -37,11 +47,10 @@ int main()
         cout << "            ~~|Create New Character|~~" << endl;
         cout << "              ~~~~~~~~~~~~~~~~~~~~~~" << endl;
 
-        cout << "Character Name: ";
+        cout << "Character Name: "; // new mane for character
         cin >> character_name;
 
-
-        cout << endl << "                 ~~|Select Job|~~ " << endl;
+        cout << endl << "                 ~~|Select Job|~~ " << endl; // new job
         
         cout << "         0 - Warrior" << "          " << "1 - Magician" << endl;
         cout << "         2 - Rouge" << "            " << "3 - Archer" << endl;
@@ -49,13 +58,13 @@ int main()
         cout << "------------------> ";
         cin >> character_job;
 
-        while(character_job < 0 || character_job > 4){
+        while(character_job < 0 || character_job > 4){ // job choise validation
             cout << "Invalid Job!, please try again" << endl;
             cout << "Select Job:";
             cin >> character_job;
         }
 
-        new_character = Character(character_name, (job)character_job);
+        new_character = Character(character_name, (job)character_job); // space allocation for new character
         main_user.insert_character(new_character,5);
     }
     else
@@ -68,7 +77,8 @@ int main()
     
     int choiseChar;
     cin >> choiseChar;
-    while( choiseChar > main_user.characterCount() || choiseChar < 0 ){
+
+    while( choiseChar > main_user.characterCount() || choiseChar < 0 ){ // character choise validation
         cout << "Invalid choice! you have " << main_user.characterCount() << " Characters" << endl <<
         "Please try again: ";
         cin >> choiseChar;
@@ -78,14 +88,16 @@ int main()
     main_user.ChooseCharacter(the_chosen_one, choiseChar-1);
     int MenuChoose = 0;
 
-    while( MenuChoose != 9)
+    while( MenuChoose != 9) // character choise Menu
     {
         MenuChoose = 0;
         cout << endl << endl << "Please choose option:" << endl <<
             "1 - Attack" << endl <<
             "2 - Character experience ststus" << endl <<
-            "3 - Switch character" << endl;
+            "3 - Switch character" << endl <<
+            "0 - Exit program" << endl ;
         cin >> MenuChoose;
+
         switch(MenuChoose)
         {
             case 1: // Attak
@@ -142,9 +154,16 @@ int main()
                 break;
             }
 
+            case 0:
+            {
+                cout << "bye! :)" << endl;
+                return 0;
+            }
+
             default:
             {
-                return 0;
+                cout << "Wrong chiose! please try again" << endl << endl ;
+                break;
             }
         }
     }
